0009672407c409cd2ad81a9e0c5cbb950959c39f
isklBlue <76448908+isklBlue@users.noreply.github.com>
2022-05-04 20:36:11 -0500

    finished content and animations

--- scroll.js
+++ scroll.js
@@ -4,24 +4,19 @@ function pathPrepare ($el) {
   $el.css("stroke-dashoffset", lineLength);
 }
 
-var $word = $("path#word");
-var $dot = $("path#dot");
+var $tube = $("path#tube");
 
 // prepare SVG
-pathPrepare($word);
-pathPrepare($dot);
+pathPrepare($tube);
 
 // init controller
 var controller = new ScrollMagic.Controller();
 
 // build tween
 var tween = new TimelineMax()
-  .add(TweenMax.to($word, 0.9, {strokeDashoffset: 0, ease:Linear.easeNone})) // draw word for 0.9
-  .add(TweenMax.to($dot, 0.1, {strokeDashoffset: 0, ease:Linear.easeNone}))  // draw dot for 0.1
-  .add(TweenMax.to("path", 1, {stroke: "#33629c", ease:Linear.easeNone}), 0);			// change color during the whole thing
+  .add(TweenMax.to($tube, 1, { strokeDashoffset: 0, ease: Linear.easeNone }));
 
 // build scene
-var scene = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 200, tweenChanges: true})
+var scene = new ScrollMagic.Scene({ triggerElement: "#trigger", duration: 5430, offset: 2700, tweenChanges: true})
         .setTween(tween)
-        .addIndicators() // add indicators (requires plugin)
         .addTo(controller);
